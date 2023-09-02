cc607b7fac08620809fb85c8a886a677869be06a
94CJG <krsoi9412@gmail.com>
2023-09-02 18:44:07 +0900

    Header 컴포넌트 및 스타일 컴포넌트를 이용하여 마크업
    ㅁ 스타일 컴포넌트를 사용하여 마크업
    ㅁ

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,56 +1,60 @@
 import styled from "styled-components";
 
 const TopContainer = styled.div`
-	background-color: green; // 색상 변경 예정
-	color: black;
 	position: fixed;
 	top: 0;
 	left: 0;
 	width: 100%;
-	max-width: 100vw;
 	z-index: 999;
 `;
 
-const Logo = styled.img`
-	width: 65px;
-`;
-
-const HeaderDiv = styled.div`
-  display: flex;
-	width: 100%;
-`;
-
 const HdInner = styled.div`
-	padding: 1.2rem 2rem;
-	box-sizing: border-box;
+	width: 1000px;
+	display: flex;
+	align-items: center;
 	justify-content: space-between;
-	width: 100%;
+	padding: 1.2rem;
 	margin: 0 auto;
-	position: relative;
+	li {
+		text-align: center;
+		font-size: 1.2rem;
+		margin-right: 1.5rem;
+	}
 `;
 
+const NavList = styled.ul`
+	display: flex;
+`;
 
 function Header() {
 	return (
 		<div id="header">
-			<div className="hd_inner">
-				<div className="logo">
-					<a href="/">
-						<img className="logImag" src="assets/logo.png" alt="로고" />
-					</a>
+			<TopContainer>
+				<div className="hd_Inner">
+					<HdInner>
+						<div className="logo" style={{ width: '65px', margin: '0' }}>
+							<a href="/" style={{ width: '100%', display: 'block' }}>
+								<img className="logImag" src="assets/logo.png" alt="로고" style={{ width: '100%' }} />
+							</a>
+						</div>
+						<nav className="header_nav" role="navigation" aria-label="메인 메뉴">
+							<ul>
+								<NavList>
+									<li>중고거래</li>
+									<li>동네가게</li>
+									<li>알바</li>
+									<li>부동산 직거래</li>
+									<li>중고차 직거래</li>
+								</NavList>
+							</ul>
+						</nav>
+						<div className="search">
+							<input type="text" placeholder="물품이나 동네를 검색 해보세요" />
+							<button>채팅하기</button>
+						</div>
+					</HdInner>
 				</div>
-				<nav className="header_nav" role="navigation" aria-label="메인 메뉴">
-					<ul>
-						<li>중고거래</li>
-						<li>동네가게</li>
-						<li>알바</li>
-						<li>부동산 직거래</li>
-						<li>중고차 직거래</li>
-					</ul>
-				</nav>
-				<input type="text" placeholder="물품이나 동네를 검색 해보세요" />
-				<button>채팅하기</button>
-			</div>
+			</TopContainer>
 		</div>
 	);
 }
@@ -61,5 +65,6 @@ export default Header;
  * 오류: 글로벌 스타일 컴포넌트가 적용이 안됨.
  * 해결: 전체적인 태그를 적용 후 그 안에 작성 해주기.
  * 
- * 형조에게 PR 날리기
+ * 오류: 'React'는 UMD 전역을 참조하지만 현재 파일은 모듈입니다.
+ * 해결: import React from "react"; 및 tsconfig.json또는 의 jsx컴파일러 옵션이 있어야 한다.
  */
