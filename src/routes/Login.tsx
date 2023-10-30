3512d179e0f0254cb3990bd36fd426d393ae304a
94CJG <krsoi9412@gmail.com>
2023-10-30 20:25:15 +0900

    CSS를 이용하여 로그인 페이지를 꾸미는중
    ㅁ 문제는 로그인 유효성 검사 실패 했을 때 얼랏창으로 경고 메세지로 교체 해야 할것 같다.
    ㅁ display -> flex를 이용하여 작업중인데, 글이 길어지면서 이쁘게 안된다.

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,5 +1,39 @@
-// import styled from "styled-components";
+import styled from "styled-components";
 import { useForm } from 'react-hook-form';
+
+//css 
+
+const All = styled.div`
+	width: 1420px;
+	display: flex;
+	justify-content: center;
+	padding-top: 100px;
+
+	.login-wrap {
+		display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+		width: 750px;
+		height: 450px;
+		border-radius: 30px;
+    border: 2px solid red;
+	}
+	.title {
+		padding-bottom: 22px;
+		font-size: 30px;
+	}
+`;
+
+const InpuutP = styled.p`
+	padding: 10px 0;
+`;
+
+const InputSize = styled.input`
+	width: 600px;
+	height: 40px;
+	margin-bottom: 22px;
+`;
 
 interface IForm {
 	userName: string;
@@ -12,59 +46,62 @@ function Login() {
 		console.log(data);
 	};
 	return (
-		<div>
-			<h1>로그인을 해주세요.</h1>
-			<form onSubmit={handleSubmit(onValid)}>
-				<p>
-					<span>아이디</span>
-				</p>
-				<input {...register("userName",
-					{
-						pattern: {
-							message: "아이디는 a~z, 0~9 조합 및 10글자 이하로 하여 만들 수 있으며, 특수문자는 사용 불가능 합니다.",
-							value: /^[a-z0-9]+$/
-						},
-						required: "아이디는 필수 항목입니다.",
-						minLength: {
-							value: 4,
-							message: "아이디는 최소 4글자 이상이어야 합니다.",
-						},
-						maxLength: {
-							value: 10,
-							message: "아이디는 최대 10글자까지 허용됩니다.",
-						},
-					})
-				}
-					placeholder="ID"
-				/>
-				<p>{errors.userName && errors.userName.message}</p>
+		<All >
+			<div className="login-wrap">
+				<h1 className="title">로그인을 해주세요.</h1>
+				<form onSubmit={handleSubmit(onValid)}>
+					<InpuutP>
+						<span>아이디</span>
+					</InpuutP>
 
-				<p>
-					<span>비밀번호</span>
-				</p>
-				<input {...register("password",
-					{
-						pattern: {
-							message: "비밀번호는 a~z, 0~9, 특수문자 !,@,#,^ 조합하여 만들 수 있습니다.",
-							value: /^[a-zA-Z0-9!@#^]+$/
-						},
-						required: "비밀번호는 필수 항목 입니다.",
-						minLength: {
-							value: 4,
-							message: "비밀번호는 최소 4글자 이상이어야 합니다.",
-						},
-						maxLength: {
-							value: 10,
-							message: "비밀번호는 최대 10글자까지 허용됩니다.",
-						},
-					})
-				}
-					placeholder="PW"
-				/>
-				<p>{errors.password && errors.password.message}</p>
-				<button type='submit'>로그인하기</button>
-			</form>
-		</div>
+					<InputSize {...register("userName",
+						{
+							pattern: {
+								message: "아이디는 a~z, 0~9 조합 및 10글자 이하로 하여 만들 수 있으며, 특수문자는 사용 불가능 합니다.",
+								value: /^[a-z0-9]+$/
+							},
+							required: "아이디는 필수 항목입니다.",
+							minLength: {
+								value: 4,
+								message: "아이디는 최소 4글자 이상이어야 합니다.",
+							},
+							maxLength: {
+								value: 10,
+								message: "아이디는 최대 10글자까지 허용됩니다.",
+							},
+						})
+					}
+						placeholder="ID"
+					/>
+					<p>{errors.userName && errors.userName.message}</p>
+
+					<InpuutP>
+						<span>비밀번호</span>
+					</InpuutP>
+					<InputSize {...register("password",
+						{
+							pattern: {
+								message: "비밀번호는 a~z, 0~9, 특수문자 !,@,#,^ 조합하여 만들 수 있습니다.",
+								value: /^[a-zA-Z0-9!@#^]+$/
+							},
+							required: "비밀번호는 필수 항목 입니다.",
+							minLength: {
+								value: 4,
+								message: "비밀번호는 최소 4글자 이상이어야 합니다.",
+							},
+							maxLength: {
+								value: 10,
+								message: "비밀번호는 최대 10글자까지 허용됩니다.",
+							},
+						})
+					}
+						placeholder="PW"
+					/>
+					<p>{errors.password && errors.password.message}</p>
+					<button type='submit'>로그인하기</button>
+				</form>
+			</div>
+		</All>
 	);
 }
 
